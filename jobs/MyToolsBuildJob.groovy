f2b4c230aff497ddd4c609851b2c0662f10f7bc0
satya-prakash-23 <satyaprakash23@gmail.com>
2016-08-03 00:54:03 -0500

    Updated MyToolsBuildJob build step

--- jobs/MyToolsBuildJob.groovy
+++ jobs/MyToolsBuildJob.groovy
@@ -1,9 +1,12 @@
 
-job('My Tools Build') {
+job('my-tools-Build') {
     scm{
         github 'pyxius/my-tools'
     }
     triggers{
         scm 'H/5 * * * *'
+    }
+    steps {
+        cmd ('sbt clean compile package')
     }
 }
